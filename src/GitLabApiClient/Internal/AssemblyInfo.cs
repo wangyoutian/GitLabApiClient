acd27985aad7e60abaf4808f1a4bd39d9f584056
WANG,Youtian <wangyoutian@nilnul.com>
2023-06-14 20:38:26 +0800

    strong named; pushed;

--- src/GitLabApiClient/Internal/AssemblyInfo.cs
+++ src/GitLabApiClient/Internal/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("GitLabApiClient.Test")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+//[assembly: InternalsVisibleTo("GitLabApiClient.Test")]
+//[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
